6bc426f939bc0855d6306610e314043b4c5b34b0
Chekkal <chekkal@gmail.com>
2018-04-04 10:38:51 +0100

    Initialization

---
+++ src/main/java/com/sqli/challenge/EcommerceFacade.java
@@ -0,0 +1,40 @@
+package com.sqli.challenge;
+
+import java.util.List;
+
+public class EcommerceFacade {
+    public void addMachine(String name, int quantity, int unitPrice) {
+    }
+
+    public void removeMachine(String name, int quantity) {
+    }
+
+    public void addCapsule(String name, int quantity, int unitPrice) {
+    }
+
+    public String cartContent() {
+        return null;
+    }
+
+    public EcommerceFacade order() {
+        return this;
+    }
+
+    public boolean hasErrors() {
+        return false;
+    }
+
+    public String errors() {
+        return null;
+    }
+
+    public void voucher(String code) {
+    }
+
+    public void removeCapsule(String name, int quantity) {
+    }
+
+    public String summary() {
+        return null;
+    }
+}
